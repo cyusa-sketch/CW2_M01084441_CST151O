8df25f928c8dfe892d318369ec5f13c5397af27d
cyusa-sketch <131197488+cyusa-sketch@users.noreply.github.com>
2025-12-15 23:12:42 +0400

    Refactor app.py to update navigation and remove migrations
    Removed user and incident migration calls and updated navigation structure.

--- app.py
+++ app.py
@@ -1,33 +1,25 @@
 import streamlit as st
-import sqlite3
 from app.data.db import connect_database
 from app.data.schema import create_all_tables
-from app.services.user_service import migrate_users_from_file
-from app.data.incidents import migrate_incidents_from_file
 
+# connect to the database and ensure all tables exist
 conn = connect_database()
 create_all_tables(conn)
 conn.close()
 
-# Migrate users from file
-migrate_users_from_file()
-
-# Migrate incidents from file
-migrate_incidents_from_file()
-
-pg = st.navigation(
+# the main navigation bar
+nav = st.navigation(
     [
-        st.Page("pages/Dashboard.py"),
-        st.Page("pages/Login.py"),
-        st.Page("pages/Register.py"),
-        st.Page("pages/Analyzer.py"),
-        st.Page("pages/Cyber.py"),
+        st.Page("pages/Login.py", title="Login"),
+        st.Page("pages/Register.py", title="Register"),
+        st.Page("pages/Dashboard.py", title="Dashboard"),
+        st.Page("pages/Cyber.py", title="Cyber View"),
+        st.Page("pages/Analyzer.py", title="AI Analyzer"),
     ]
 )
 
 st.sidebar.page_link("pages/Dashboard.py", label="Dashboard")
-st.sidebar.page_link("pages/Analyzer.py", label="AI Incident Analyzer")
-st.sidebar.page_link("pages/Cyber.py", label="Cyber Dashboard")
+st.sidebar.page_link("pages/Cyber.py", label="Cyber View")
+st.sidebar.page_link("pages/Analyzer.py", label="AI Analyzer")
 
-
-pg.run()
+nav.run()
